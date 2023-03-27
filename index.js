ee663fa49e12d5d792d9fca0a1eccaa3627ac8b6
Vidar Åsberg <vidar.asberg@protonmail.com>
2023-03-27 16:38:50 +0200

    Uses process.env.PORT as default

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ const server = createServer((req, res) => {
   res.end();
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
